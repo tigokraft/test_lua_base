934e847f44a7f4fea0ff27b8fe94e3cfd9065f41
tigokraft <77751777+tigokraft@users.noreply.github.com>
2023-08-11 00:31:23 +0100

    Update base.lua

--- base.lua
+++ base.lua
@@ -471,14 +471,14 @@ function OrionLib:MakeWindow(WindowConfig)
 	local UIHidden = false
 
 	WindowConfig = WindowConfig or {}
-	WindowConfig.Name = WindowConfig.Name or "testing"
+	WindowConfig.Name = WindowConfig.Name or "Leaks/Scripts Hub"
 	WindowConfig.ConfigFolder = WindowConfig.ConfigFolder or WindowConfig.Name
 	WindowConfig.SaveConfig = WindowConfig.SaveConfig or false
 	WindowConfig.HidePremium = WindowConfig.HidePremium or false
 	if WindowConfig.IntroEnabled == nil then
 		WindowConfig.IntroEnabled = true
 	end
-	WindowConfig.IntroText = WindowConfig.IntroText or "testing"
+	WindowConfig.IntroText = WindowConfig.IntroText or "Leaks/Scripts Hub"
 	WindowConfig.CloseCallback = WindowConfig.CloseCallback or function() end
 	WindowConfig.ShowIcon = WindowConfig.ShowIcon or false
 	WindowConfig.Icon = WindowConfig.Icon or "rbxassetid://8834748103"
